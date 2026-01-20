3beb624bd899f4f062083abc1d1810e6cbf7616c
Yuri <ygonzoo@gmail.com>
2026-01-20 17:57:55 +0100

    change @astrojs/node for @astrojs/vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import vercelServerless from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercelServerless({}),
 });
